fd3a251c4c024350d7595011b1c5382af103aeb2
Alex Brenyo <tech.metrofeed@outlook.com>
2026-01-20 00:11:34 -0500

    Auto update from script

--- Louisville/routes_index.js
+++ Louisville/routes_index.js
@@ -128,10 +128,16 @@ const ROUTES = {
             "dir1": "Inbound"
       },
       {
+            "id": "94",
+            "label": "94 \u2013 UofL Cardinal Shuttle",
+            "dir0": "",
+            "dir1": "Inbound"
+      },
+      {
             "id": "99",
             "label": "99 \u2013 UPS West Louisville",
             "dir0": "Outbound",
-            "dir1": ""
+            "dir1": "Inbound"
       }
 ],
     railRoutes: []
