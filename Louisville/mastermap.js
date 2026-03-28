417031099cf99def0757a919ff2b3ad917ad6134
Alex Brenyo <tech.metrofeed@outlook.com>
2026-03-28 15:11:33 -0400

    Auto update from script

--- Louisville/mastermap.js
+++ Louisville/mastermap.js
@@ -1,6 +1,6 @@
 /* Auto-generated: mastermap.js (data core) */
 // City: Louisville
-// Generated: 2026-03-28T14:10:05.115504-04:00
+// Generated: 2026-03-28T15:10:04.690717-04:00
 
 const masterRoutes = [
   {
