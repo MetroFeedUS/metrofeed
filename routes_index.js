5530f801440bcdb05ef8aa6f0097c8e7fa8063ab
Alex Brenyo <tech.metrofeed@outlook.com>
2026-04-16 13:55:19 -0400

    UI update Thu 04/16/2026 13:55:19.46

---
+++ routes_index.js
@@ -0,0 +1,469 @@
+window.ROUTES = {
+  "cincinnati": {
+    "busRoutes": [
+      {
+        "id": "sorta_1",
+        "label": "[SORTA] 1 - Mt. Adams",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_1",
+        "label": "[TANK] 1 - Dixie Hwy/Florence",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_2",
+        "label": "[SORTA] 2 - Madeira Commuter",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_201",
+        "label": "[TANK] 2X - Airporter",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_3",
+        "label": "[SORTA] 3X - Montgomery Express",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_3",
+        "label": "[TANK] 3 - Ludlow/Bromley",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_4",
+        "label": "[SORTA] 4 - Montgomery Road",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_5",
+        "label": "[SORTA] 5 - Blue Ash",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_5",
+        "label": "[TANK] 5 - Holman Ave/Fort Wright",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_6",
+        "label": "[SORTA] 6 - Queen City Avenue",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_7",
+        "label": "[TANK] 7 - Madison Ave/Latonia",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_8",
+        "label": "[SORTA] 8 - Blue Ash / Silverton Commuter",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_8",
+        "label": "[TANK] 8 - Eastern Ave/Crestview Hills",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_10",
+        "label": "[SORTA] 10 - Madison Rd/ Erie Ave",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_11",
+        "label": "[SORTA] 11 - Madison Road",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_12",
+        "label": "[SORTA] 12 - Madisonville Commuter",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_12",
+        "label": "[TANK] 12 - Bellevue/Dayton",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_16",
+        "label": "[SORTA] 16 - Spring Grove / Daly",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_16",
+        "label": "[TANK] 16 - West Newport/Fort Thomas",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_17",
+        "label": "[SORTA] 17 - Hamilton Avenue",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_19",
+        "label": "[SORTA] 19 - Colerain Avenue",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_20",
+        "label": "[SORTA] 20 - Winton Road",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_21",
+        "label": "[SORTA] 21 - Harrison Avenue",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_22",
+        "label": "[SORTA] 22 - Glenway / Madison",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_23",
+        "label": "[SORTA] 23X - Forest Park Express",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_24",
+        "label": "[SORTA] 24 - MLK / Beechmont",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_24",
+        "label": "[TANK] 24 - NKU Shuttle",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_25",
+        "label": "[SORTA] 25 - Mt. Lookout Commuter",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_25",
+        "label": "[TANK] 25 - NKU/Alexandria",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_2501",
+        "label": "[TANK] 25X - Alexandria Express",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_27",
+        "label": "[SORTA] 27 - Beekman / Linn",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_28",
+        "label": "[SORTA] 28 - East End",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_29",
+        "label": "[SORTA] 29X - Milford Express",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_30",
+        "label": "[SORTA] 30 - Beechmont Commuter",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_3001",
+        "label": "[TANK] 30X - Fort Wright/Lake Park Dr Express",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_31",
+        "label": "[SORTA] 31 - Taft / McMillan",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_32",
+        "label": "[SORTA] 32 - West 8th Street",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_33",
+        "label": "[SORTA] 33 - Glenway Avenue",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_36",
+        "label": "[SORTA] 36 - Price Hill / Uptown",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_37",
+        "label": "[SORTA] 37 - MLK / Westwood Northern",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_38",
+        "label": "[SORTA] 38 - Uptown Commuter",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_3901",
+        "label": "[TANK] 39X - Petersburg Rd/South Hebron Express",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_40",
+        "label": "[SORTA] 40 - Montana Commuter",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_4001",
+        "label": "[TANK] 40X - Worldwide Blvd/North Hebron Express",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_41",
+        "label": "[SORTA] 41 - North Bend Road",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_4201",
+        "label": "[TANK] 42X - Florence Hub/Industrial Rd Express",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_43",
+        "label": "[SORTA] 43 - Reading Road",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_46",
+        "label": "[SORTA] 46 - Avondale",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_49",
+        "label": "[SORTA] 49 - Fairmount",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_50",
+        "label": "[SORTA] 50 - River Road Commuter",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_51",
+        "label": "[SORTA] 51 - Uptown Connector",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_52",
+        "label": "[SORTA] 52X - Harrison Express",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_61",
+        "label": "[SORTA] 61 - Galbraith Road",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_64",
+        "label": "[SORTA] 64 - Westwood",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_64",
+        "label": "[TANK] 64 - 64",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_65",
+        "label": "[SORTA] 65 - Western Hills",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_67",
+        "label": "[SORTA] 67 - Kemper Road",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_71",
+        "label": "[SORTA] 71X - Kings Island Express",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_74",
+        "label": "[SORTA] 74X - Colerain Express",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_75",
+        "label": "[SORTA] 75X - Anderson Express",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_77",
+        "label": "[SORTA] 77 - Delhi",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_78",
+        "label": "[SORTA] 78 - Vine Street",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_81",
+        "label": "[SORTA] 81 - Mt. Washington Commuter",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_82",
+        "label": "[SORTA] 82X - Eastgate Express",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_100",
+        "label": "[SORTA] 100 - Streetcar - OTR - Banks",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_352",
+        "label": "[TANK] 352 - Holmes HS #3 PM",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_450",
+        "label": "[TANK] 450 - Holmes HS #2 PM",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_451",
+        "label": "[TANK] 451 - Holmes HS #2 AM",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_485",
+        "label": "[TANK] 485 - Holmes HS #5 PM",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_552",
+        "label": "[TANK] 552 - Holmes HS #1 AM",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_553",
+        "label": "[TANK] 553 - Holmes HS #6 PM",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_652",
+        "label": "[TANK] 652 - Holmes HS #8 AM",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_751",
+        "label": "[TANK] 751 - Holmes HS #7 PM",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_752",
+        "label": "[TANK] 752 - Holmes HS #9 PM",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_851",
+        "label": "[TANK] 851 - Holmes HS #4 PM",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "tank_3701",
+        "label": "[TANK] 3701 - Southbank Shuttle",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      },
+      {
+        "id": "sorta_90",
+        "label": "[SORTA] M+ - MetroPlus",
+        "dir0": "Outbound",
+        "dir1": "Inbound"
+      }
+    ],
+    "railRoutes": []
+  }
+};
